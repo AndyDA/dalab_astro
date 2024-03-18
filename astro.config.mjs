bdfeadc06745df5134d027f8e13d7c1611cd8c30
Andy <andy@da.md>
2024-03-18 23:38:27 +0200

    some little changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://andyda.github.io',
-  base: '/astro_dart',
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: "https://andyda.github.io",
 });
